9d190f00c21b80eaee1d56852074e8dbdf43e951
thomasvergne <thomas.vergne.contact@gmail.com>
2023-06-21 22:52:52 +0200

    fix: removed function specific type from CST.

--- src/Language/Esperanza/CST/Modules/Type.hs
+++ src/Language/Esperanza/CST/Modules/Type.hs
@@ -2,6 +2,7 @@ module Language.Esperanza.CST.Modules.Type where
 
 import           Prelude hiding (Type)
 import Language.Esperanza.CST.Modules.Namespaced (Namespaced)
+import Language.Esperanza.CST.Modules.Located (Located)
 
 data Type a
   = TypeInt
@@ -31,23 +32,11 @@ data Type a
   --   The argument is the name of the type variable
   --   For example, a is represented as
   --   TypeVar "a"
-  | TypeRefinement (Text, Type a) (Maybe a)
+  | TypeRefinement (Text, Type a) (Maybe (Located a))
   -- ^ TypeRefinement denotes a type refinement, e.g. (x: Int | x > 0)
   --   The first argument is the name of the refinement variable
   --   The second argument is the type of the refinement variable
   --   The third argument is the refinement predicate
   --   For example, (x: Int | x > 0) is represented as
   --   TypeRefinement ("x", TypeInt) (Just (x > 0))
-  | TypeFunction [Type a] (Type a)
-  -- ^ TypeFunction denotes a function type, e.g. Int -> Int
-  --   The first argument is a list of argument types
-  --   The second argument is the return type
   deriving (Eq, Show, Ord)
-
-data Class a
-  = Class Namespaced [Type a]
-  deriving (Eq, Show)
-
-data TypeScheme a
-  = TypeForall [Class a] (Type a)
-  deriving (Eq, Show)
