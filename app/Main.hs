780644bc9ebea3ba5aac32d811d9f55619e8276c
thomasvergne <thomas.vergne.contact@gmail.com>
2023-06-21 22:56:30 +0200

    feat: added necessary calls to main entry.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,28 @@
 module Main where
-import qualified Language.Esperanza.Parser as P
-import qualified Language.Esperanza.CST.Toplevel as C
+
+import           Language.Esperanza.AST.Conversion      (convertToplevel)
 import qualified Language.Esperanza.CST.Modules.Located as C
+import qualified Language.Esperanza.CST.Toplevel        as T
+import           Language.Esperanza.Module              (runModuleBundlingPass,
+                                                         runModuleResolvingPass)
+import qualified Language.Esperanza.Parser              as P
 
 main :: IO ()
 main = do
   let file = "examples/main.esp"
   x <- decodeUtf8 <$> readFileBS file
-  let ast = P.parseEsperanza file x
-  case ast of
-    Right ast -> print ast
+  let result = P.parseEsperanza file x
+  case result of
+    Right (C.Located _ (T.TModule "Main" toplevels)) -> do
+      resolved <- runModuleResolvingPass "examples" toplevels
+      case resolved of
+        Right ast' -> do
+          bundled <- runModuleBundlingPass ast'
+          case bundled of
+            Right ast'' -> do
+              let ast''' = map convertToplevel ast''
+              print ast'''
+            Left err -> print err
+        Left err -> print err
     Left err -> print err
+    _ -> print ("Invalid file" :: String)
